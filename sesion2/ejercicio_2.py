f7eb9f8bad76a3f3e34abae53972396c332bd9c0
dfloresgonz <dfloresgonz@gmail.com>
2024-04-13 11:05:08 -0500

    refactor

--- sesion2/ejercicio_2.py
+++ sesion2/ejercicio_2.py
@@ -3,8 +3,8 @@ y1=int(input("Type y1: "))
 x2=int(input("Type x2: "))
 y2=int(input("Type y2: "))
 
-distance=((x2-x1)**2) + ((y2-y1)**2)
+distance = ((x2-x1)**2) + ((y2-y1)**2)
 
-distance = distance**0.5
+distance = distance**0.5 #square root o raiz cuadrada
 
 print("The distance is: ", distance)
