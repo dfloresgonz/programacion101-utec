1af10aa4c3dd2affdea8bef614cb221be88f603c
dfloresgonz <dfloresgonz@gmail.com>
2024-04-13 11:42:38 -0500

    ejercicio 10

---
+++ sesion2/ejercicio_10.py
@@ -0,0 +1,10 @@
+AMOUNT_1_LITTER=1.25
+AMOUNT_1MORE_LITTER=3.75
+
+# change these variables for different outputs :)
+cant_1_litter_bottles=12
+cant_1more_litter_bottles=29
+
+total = (cant_1_litter_bottles * AMOUNT_1_LITTER) + (cant_1more_litter_bottles * AMOUNT_1MORE_LITTER)
+
+print("total $", total)
