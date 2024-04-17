526918e417ec66f33d114be1f0784ca3a63f109a
dfloresgonz <dfloresgonz@gmail.com>
2024-04-16 19:13:27 -0500

    next 2 exercizes

---
+++ set-ejercicios1/ejerc009.py
@@ -0,0 +1,8 @@
+a = int(input("type Number 1: "))
+b = int(input("type Number 2: "))
+c = int(input("type Number 3: "))
+d = int(input("type Number 4: "))
+
+response = (a + b) * (c - d)
+
+print(response)
